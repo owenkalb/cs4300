4d3be1dd725758e3e97950fb6f9dd600d4ecd181
Owen Kalbfleisch <oakalbfleisch@gmail.com>
2025-09-23 20:23:59 +0000

    task 6 complete

--- homework1/src/task6.py
+++ homework1/src/task6.py
@@ -1,1 +1,7 @@
-#testtask6
+# Reads task6_read_me.txt and counts the number of words in it.
+def count_words_in_file(file_path):
+    file_path = 'task6_read_me.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+        words = text.split()
+        return len(words)
--- homework1/tests/test_task6.py
+++ homework1/tests/test_task6.py
@@ -1,1 +1,8 @@
+import pytest
+from task6 import count_words_in_file
+
+def test_word_count_task6_read_me():
+    file_path = 'task6_read_me.txt'
+    expected_word_count = 104
+    assert count_words_in_file(file_path) == expected_word_count
 
