8e2eab230085ee03cd3bf82020d6288ba3ca17d4
Owen Kalbfleisch <oakalbfleisch@gmail.com>
2025-09-23 20:30:16 +0000

    Task7 done

--- homework1/src/task7.py
+++ homework1/src/task7.py
@@ -1,1 +1,9 @@
+# Use pip to install package of your choice and write script
+import numpy as np
 
+# Matrix Multiplication
+def matrix_multiply(a, b):
+    matrix_a = np.array(a)
+    matrix_b = np.array(b)
+    result = np.dot(matrix_a, matrix_b)
+    return result
--- homework1/tests/test_task7.py
+++ homework1/tests/test_task7.py
@@ -1,1 +1,15 @@
+import numpy as np
+from task7 import matrix_multiply
 
+# mult a 2x2
+def test_matrix_multiply_2x2():
+    a = [[1, 2], [3, 4]]
+    b = [[2, 0], [1, 2]]
+    expected = np.array([[4, 4], [10, 8]])
+    np.testing.assert_array_equal(matrix_multiply(a, b), expected)
+# mult a 2x3 by 3x2
+def test_matrix_multiply_2x3_3x2():
+    a = [[1, 2, 3], [4, 5, 6]]
+    b = [[7, 8], [9, 10], [11, 12]]
+    expected = np.array([[58, 64], [139, 154]])
+    np.testing.assert_array_equal(matrix_multiply(a, b), expected)
